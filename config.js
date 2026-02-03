bce39633362ec3e24c2704df89da25b3c72fb7d9
nexusthede <ivanrrr76@gmail.com>
2026-02-03 15:14:09 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   TOKEN: process.env.TOKEN,               // Bot token from .env
-  PREFIX: ",",                            // Command prefix
+  PREFIX: ".",                            // Command prefix
   GUILD_ID: "1449708401050259457",             // Your server ID
   CATEGORY_ID: "1468344769213235306",   // Voice Master category ID
   JOIN_TO_CREATE_ID: "1468360353955053669", // Join-to-create VC ID
