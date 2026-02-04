aaa9bacbd0bc8fe8bc7e4cac576a135c0f0b55c3
nexusthede <ivanrrr76@gmail.com>
2026-02-03 18:32:34 -0800

    Update config.js with new channel ID and color
    Added WELCOME_CHANNEL_ID and updated EMBED_COLOR.

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-  TOKEN: process.env.TOKEN,               // Bot token from .env
-  PREFIX: ".",                            // Command prefix
+  TOKEN: process.env.TOKEN,                     // Bot token from .env
+  PREFIX: ".",                                  // Command prefix
   GUILD_ID: "1449708401050259457",             // Your server ID
-  CATEGORY_ID: "1468344769213235306",   // Voice Master category ID
-  JOIN_TO_CREATE_ID: "1468360353955053669", // Join-to-create VC ID
-  EMBED_COLOR: "#00008B"
+  CATEGORY_ID: "1468344769213235306",          // Voice Master category ID
+  JOIN_TO_CREATE_ID: "1468360353955053669",    // Join-to-create VC ID
+  WELCOME_CHANNEL_ID: "1466485294927843338",   // Channel where welcome embed is sent
+  EMBED_COLOR: "#000001"                        // Black embed color for all embeds
 };
