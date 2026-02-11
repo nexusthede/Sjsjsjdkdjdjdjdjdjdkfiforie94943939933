60c08485c77c9670e3fa7b92d17e46cd5f52b609
nexusthede <ivanrrr76@gmail.com>
2026-02-11 10:49:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-  TOKEN: process.env.TOKEN,                   // Bot token
-  PREFIX: ".",                                // Command prefix
-  GUILD_ID: "1449708401050259457",           // Your server ID
-  CATEGORY_ID: "1468344769213235306",        // Voice Master category ID
-  JOIN_TO_CREATE_ID: "1468360353955053669",  // Join-to-create VC ID
-  EMBED_COLOR: "#202225"                      // Dark embed color for VC & LB
+  TOKEN: process.env.TOKEN,                  // Bot token from .env
+  PREFIX: ".",                               // Command prefix
+  GUILD_ID: "1449708401050259457",          // Your server ID
+  CATEGORY_ID: "1468344769213235306",       // Voice Master category ID
+  JOIN_TO_CREATE_ID: "1468360353955053669", // Join-to-create VC ID
+  EMBED_COLOR: "#202225"                     // Dark embed color for VC & LB
 };
